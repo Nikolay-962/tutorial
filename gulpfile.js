3911aa5fc90ddfda8fa49d74e8ba9ef3b1e94564
Nikolay-962 <nikpronin62@gmail.com>
2020-10-27 22:42:19 +0300

    подключил картинки
    В галп файле подключил картинки

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,25 @@ var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var sass = require('gulp-sass');
 var browsersync = require('browser-sync');
+var image = require('gulp-image');
 var del = require('del');
 var reload = browsersync.reload;
 
 var path = {
   src: {
     html: 'src/*.html',
-    styles: 'src/styles/*.scss'
+    styles: 'src/styles/*.scss',
+    img: 'src/img/**/*.*',
   },
   build: {
     html: 'build/',
-    styles: 'build/css/'
+    styles: 'build/css/',
+    img: 'build/img/',
   },
   watch: {
     html: 'src/**/*.html',
-    styles: 'src/styles/**/*.scss'
+    styles: 'src/styles/**/*.scss',
+    img: 'src/img/**/*.*',
   },
   base: './build'
 };
@@ -54,13 +58,23 @@ function styles() {
     .pipe(reload({ stream: true }));
 };
 
+function image() {
+  return gulp
+    .src(path.src.img)
+    .pipe(canged(path.build.img))
+    .pipe(gulp.dest(path.build.img))
+    .pipe(reload({ stream: true }))
+}
+
 function watchFiles() {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.styles], styles);
+  gulp.watch([path.watch.img], image);
 };
 
+gulp.task('image', image);
 gulp.task('html', html);
 gulp.task('styles', styles);
 
-gulp.task('build', gulp.series(clean, gulp.parallel(html, styles)));
+gulp.task('build', gulp.series(clean, gulp.parallel(html, image, styles)));
 gulp.task('watch', gulp.parallel(watchFiles, browserSync));
